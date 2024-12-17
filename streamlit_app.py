5cbce7a5e27fb5102491c6734aaf466ca08f7f50
Vikos69 <86858056+Vikos69@users.noreply.github.com>
2024-12-17 15:40:30 -0600

    Update streamlit_app.py
     Let's Call the SmoothieFroot API from Our SniS App!

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,' + name_on_order, icon="✅")
 
-##    ("Banana", "Strawberries", "Peaches"),
-##)
-##st.write("Your favorite fruit is:", option)
+#🥋 Let's Call the SmoothieFroot API from Our SniS App!
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
